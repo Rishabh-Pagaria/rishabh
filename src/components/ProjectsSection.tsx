2e5ec5cb4ae4da5328b7fc10f1e2efb812497ddf
Rishabh-Pagaria <rpagaria2000@gmail.com>
2025-07-19 16:57:15 -0500

    updating the project section role

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -7,8 +7,8 @@ const ProjectsSection: React.FC = () => {
       thumbnail: 'images/Rakuun.png',
       title: 'Rakuun',
       description: 'Rakunn is a chrome extension that allows users to select text on any webpage and smartly extracting the recipeints email, generating a subject and personalized email body, and you can send the email directly from the extension, just a button click away.',
-      role: 'Writing personalized emails is always been a tedious and long task, and thus Rakuun aims to simplify this process by automating email composition. Thus, to making everyone life more simpler my work focuses on developing this extension using Next and JavaScript. Also, optimization from data retrieval to LLM integration and speed has been a key focus area.',
-      services: ['Full Stack Development using Next.js', 'GenAI', 'Google Cloud', 'Google Authentication','REST API', 'Database using Supabase','Branding & Marketing'],
+      role: 'Writing personalized emails is always been a tedious and long task, and thus Rakuun aims to simplify this process by automating email composition. So, here I worked as a Full Stack Developer using Next.js, Google Cloud for Authentication, Gemini API for GenAI to generate and fetch email, and Supabase as Database to store the tokens for user authentication.',
+      services: ['Full Stack Development using Next.js', 'GenAI', 'Google Cloud', 'Google Authentication','Google gemini API', 'Database using Supabase','Branding & Marketing'],
       demoLink: '#',
       githubLink: 'https://github.com/Rishabh-Pagaria/rakuun'
     },
@@ -16,8 +16,8 @@ const ProjectsSection: React.FC = () => {
       thumbnail: 'images/EventNXT.png',
       title: 'EventNXT',
       description: 'The customer is looking for an event management application similar to Ticketmaster, but specifically designed for the fashion industry. The platform enables users to manage guest lists, ticket sales, RSVPs, customer rewards, and send emails in one centralized space.',
-      role: 'The project was developed as part of the Software Engineering final project where the client was FashioNXT, where we were supposed to work on legacy codebase, debugging, and implementing new features using agile methodologies.',
-      services: ['Full Stack Development using Ruby on Rails', 'Software Testing', 'Project Management', 'Scrum Master', 'Pitch deck Creation and Presentation'],
+      role: 'This project being a legacy project was developed as part of my Software Engineering course where the client was FashioNXT from Fashion Tech, I worked as software developer, driving client calls, and leading the team using agile methodologies. This legacy project we developed using Ruby on Rails, PostgreSQL, Rspec & Cucumber for Software Testing, and Web Hosting on Heroku.',
+      services: ['Full Stack Development using Ruby on Rails', 'Unit Testing using Rspec & Cucumber', 'Project Management using Linear - an agile methodology tool', 'Web Hosting using Heroku', 'Pitch Creation and Presentation'],
       demoLink: 'https://www.youtube.com/watch?v=h4WR0lMknIk',
       githubLink: 'https://github.com/FashioNXT/EventNXT-Fall2024/tree/main'
     },
@@ -25,7 +25,7 @@ const ProjectsSection: React.FC = () => {
       thumbnail: 'images/seatFlex.png',
       title: 'SeatFlex',
       description: 'As part of the TAMUHACK 2025 hackathon, our team developed an Airline Overbooking Volunteer System as American Airline Challenge. This application is part of a MVP that allows users to volunteer for overbooked flights, helping airlines manage passenger loads efficiently.',
-      role: 'I led a team of 3 to work on this project in a 2 day Hacathon Setting. Our work involved from ideation, development of MVP and presenting the solution to the judges. The project was built using HTML, CSS, and JavaScript. Won the Best Begineer Software Category with a $200 worth JBL Speakers as a prize. Furthermore, working on this project to add authetnication, databse and other features to make it a full fledged application.',
+      role: 'Led a team of 3 to work on this project in a 2 day Hacathon Setting. My team was involved from ideation, development of MVP and presenting the solution to the judges. The project was built using HTML, CSS, and JavaScript. Won the Best Begineer Software Category with a $200 worth JBL Speakers as a prize. Furthermore, working on this project to add authentication, database and other features to make it a full fledged application.',
       services: ['Frontend Development through JavaScript', 'UI/UX using Figma', 'Project Lead', 'Prototyping and Presentation'],
       demoLink: 'https://www.youtube.com/watch?v=Ges_APbVLbk',
       githubLink: 'https://devpost.com/software/seat-flex'
